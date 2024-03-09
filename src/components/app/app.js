7ae6255d357b3205d8a14a9520dcf9014ac622bf
maxbbkCmd <maxbbkCmd@mail.ru>
2024-03-10 02:04:28 +0300

    added: fc cookie

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,9 +13,9 @@ class App extends Component {
     super(props);
     this.state = {
         data: [
-            {name: 'John C.', salary: 800, increase: false, id: 1},
-            {name: 'Alex M.', salary: 3000, increase: true, id: 2},
-            {name: 'Carl W.', salary: 5000, increase: false, id: 3}
+            {name: 'John C.', salary: 800, increase: false, rise:true, id: 1},
+            {name: 'Alex M.', salary: 3000, increase: true,  rise:false,id: 2},
+            {name: 'Carl W.', salary: 5000, increase: false,  rise:false,id: 3}
         ]
     }
     this.maxId = 4;
@@ -35,6 +35,7 @@ addItem = (name, salary) => {
         name, 
         salary,
         increase: false,
+        rise: false,
         id: this.maxId++
     }
     this.setState(({data}) => {
@@ -45,10 +46,26 @@ addItem = (name, salary) => {
     });
 }
 
+  onToggleProp = (id, prop) => { 
+    this.setState(({data}) =>({
+      data: data.map(item => {
+        if  (item.id  === id) {
+          return {...item, [prop]: !item[prop]}
+        }
+        return item;
+      })
+    }))
+  }
+
+
+
 render() {
+  const employers = this.state.data.length;
+  const increased = this.state.data.filter(item => item.increase).length;
+
   return (
     <div className="app">
-        <AppInfo />
+        <AppInfo employers={employers} increased={increased}/>
 
         <div className="search-panel">
             <SearchPanel/>
@@ -57,7 +74,8 @@ render() {
         
         <EmployersList 
             data={this.state.data}
-            onDelete={this.deleteItem}/>
+            onDelete={this.deleteItem}
+            onToggleProp={this.onToggleProp}/>
         <EmployersAddForm onAdd={this.addItem}/>
     </div>
 );
