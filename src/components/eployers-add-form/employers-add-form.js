d7d9a75ecd69af355d2cd0e90008fe48bf6514bb
maxbbkCmd <maxbbkCmd@mail.ru>
2024-03-09 16:45:36 +0300

    added: delete

---
+++ src/components/eployers-add-form/employers-add-form.js
@@ -0,0 +1,59 @@
+import "./employers-add-form.css";
+
+import { Component } from 'react';
+
+class EmployersAddForm extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      salary: 0
+    }
+  }
+
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+
+
+  render() {
+    const {name, salary} = this.state;
+
+
+    return (
+      <div className="app-add-form">
+  
+        <h3>Добавьте нового сотрудника</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="Как его зовут?"
+            onChange={this.onValueChange}
+            name="name"
+            value={name}/>
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="ЗП в $"
+            onChange={this.onValueChange}
+            name="salary"
+            value={salary}/>
+  
+          <button
+            type="button"
+            className="btn btn-outline-light">
+            Добавить
+          </button>
+        </form>
+      </div>
+      
+    );
+  }
+};
+
+export default EmployersAddForm;
